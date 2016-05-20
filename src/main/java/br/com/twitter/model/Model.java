94347c19f7dfd13de9bd48d2e813d726a61dca4d
Abraao Francisco da Silva <esc_asilva@uolinc.com>
2016-05-20 17:54:45 -0300

    metodos de pesquisa

--- src/main/java/br/com/twitter/model/Model.java
+++ src/main/java/br/com/twitter/model/Model.java
@@ -4,9 +4,13 @@ package br.com.twitter.model;
 
 import java.util.List;
 
+import twitter4j.GeoLocation;
+import twitter4j.Paging;
 import twitter4j.Query;
 import twitter4j.QueryResult;
+import twitter4j.Relationship;
 import twitter4j.Status;
+import twitter4j.StatusUpdate;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
@@ -32,19 +36,45 @@ public class Model {
 		Twitter twitter = factory.getInstance();
 		AccessToken accessToken = loadAccessToken();
 		twitter.setOAuthAccessToken(accessToken);
-		//Status status = twitter.updateStatus("Ola Twitter!");
+		/*Status status = twitter.updateStatus("@CASSIANECANTORA  oremos pelo brasil");*/
 
 		
-		 Query query = new Query("source: " + "java");  
+		 Query query =new Query("#rtyhgfdser");
+		 query.setSince("2016-05-15");
+		 query.setUntil("2016-05-21");
+		 query.count(100);
+
+
+           
+	
+
+
+
 	      QueryResult result = twitter.search(query); 
+	      long sinceId = result.getSinceId();
 	      List<Status> tweets = result.getTweets();
+	      int i =0;
 	      for (Status status2 : tweets) {
-	    	  
-			System.out.println(status2.getText()+" "+ status2.getUser().getName()+" "+status2);
+	    	  i++;
+	    	
+			 System.out.println("indice "+i+" "+" "+" nome "+status2.getUser().getScreenName()+" geo "+status2.getGeoLocation() +" id "+  status2.getId());
 		}
 
-            
-		
+   //733444680352948224
+
+   
+	      query.setSinceId(733743543064395778l);
+       
+	      QueryResult result2 = twitter.search(query); 
+
+	      List<Status> tweets2 = result2.getTweets();
+          
+          for (Status status2 : tweets2) {
+	    	  i++;
+			 System.out.println("indice "+i+" "+" "+" nome "+status2.getUser().getScreenName()+" geo "+status2.getGeoLocation() +" id "+  status2.getId() );
+		}
+          
+          
 		
 	    
 	
